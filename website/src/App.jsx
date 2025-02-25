543479c35ccdc5833372421f84988f75db81a1d6
Ben Young <Ben@neosofia.tech>
2025-02-25 11:53:31 +0000

    add meta tags, robots.txt, favicon.ico, and responsive design to improve lighthouse compatibility score

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -4,25 +4,23 @@ import logo from './assets/Neosofia.png';
 function App() {
 
   return (
-    <header className="flex bg-white shadow-md">
-      <nav className="flex nowrap p-2">
+    <header className="flex bg-white shadow-md h-6 sm:h-10">
+      <nav className="flex nowrap">
         <div className="p-1">
-          <a href="/#" className=""><img src={logo} alt="logo" className="h-8" /></a>
+          <a href="/#" className=""><img src={logo} alt="logo" className="mx-auto h-4 sm:h-8 sm:min-w-[204px]" /></a>
         </div>
-        <div className="w-32"></div>
 
-        <div className="text-left text-red-800 flex">
-          <div className="p-1 pr-6">
-            <a href="/#" className="text-xl">Product</a>
+        <div className="p-0 w-4 sm:w-16"></div>
+
+        <div className="text-left text-red-800 flex *:pr-1 *:sm:p-2">
+          <div>
+            <a href="/#" className="text-xs sm:text-xl">Product</a>
           </div>
-          <div className="p-1 pr-6">
-            <a href="/#" className="text-xl">Blog</a>
+          <div>
+            <a href="/#" className="text-xs sm:text-xl">Blog</a>
           </div>
-          <div className="p-1 pr-6">
-            <a href="/#" className="text-xl">About</a>
-          </div>
-          <div className="p-1 pr-6">
-            <a href="/#" className="text-xl">Who</a>
+          <div>
+            <a href="/#" className="text-xs sm:text-xl">About</a>
           </div>
         </div>
       </nav>
