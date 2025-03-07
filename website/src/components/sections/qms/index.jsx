9d9f272ff4fe3a31648522eed6baa4161246c289
Ben Young <Ben@neosofia.tech>
2025-03-07 09:06:47 +0000

    improve typography styles for md conversion

--- website/src/components/sections/qms/index.jsx
+++ website/src/components/sections/qms/index.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 import { RenderMD } from "../GetMD";
 
 export const QMS = () => {
   return (
-    <section id="qms" className="min-h-screen flex py-10 justify-center items-center">
-      <div className="max-w-4xl z-10 m-5 md:m-16 prose-sm md:prose-md overflow-x-auto">
+    <section id="qms" className="min-h-screen flex justify-center items-center">
+      <div className="max-w-4xl z-10 m-6 md:m-16 prose-base md:prose-lg overflow-x-auto">
         <RenderMD />
       </div>
     </section>
