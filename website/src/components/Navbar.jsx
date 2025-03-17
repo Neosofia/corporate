af9e4d720905123eee1a33a2578fbc4187afeaa0
Ben Young <Ben@neosofia.tech>
2025-03-17 13:42:14 +0000

    update static asset location

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-import logo from '../assets/Neosofia.png';
+import logo from '../../app/assets/Neosofia.png';
 import { LoadingScreen } from "./LoadingScreen";
 
 export const Navbar = () => {
